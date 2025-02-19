0bbe4d3bab9d54035b9a1baf1eb3535dd37550d7
Grangclark <96944049+Grangclark@users.noreply.github.com>
2025-02-19 20:44:27 +0900

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by 長橋和敏 on 2025/02/19.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
